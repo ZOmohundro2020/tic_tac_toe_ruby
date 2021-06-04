1749d8946d8bde353c3ddb28f4eb548b7566bd55
ZOmohundro <zaneomohundro@gmail.com>
2021-06-04 15:58:35 -0400

    add more outlining

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,5 +1,26 @@
 class Board
   @@state = 'Player1'
+  def display_state
+    puts
+    puts "1 | 2 | 3 "
+  end
+end
+
+class Row
+  def initialize(values_array)
+    #@slot1 = values_array[0]
+    #@slot2 = values_array[1]
+    #@slot3 = values_array[2]
+    p "@slot1 is #{@slot1}"
+    
+    
+  end
+  def display_values
+    # p @slot1
+    #values_array
+    puts "test"
+  end
+
 end
 
 class Player
@@ -10,8 +31,23 @@ class Player
   end
 end
 
-player1 = Player.new("Player1","X")
-player2 = Player.new("Player2","O")
 
-p player1.name
-p player1
+
+
+def main()
+  board = Board.new()
+  row1 = [7,8,9]
+  
+  player1 = Player.new("Player1","X")
+  player2 = Player.new("Player2","O")
+
+
+  puts "\nWelcome to tic-tac-toe"
+  puts "#{player1.name} will start and their piece is '#{player1.piece}'"
+
+  board.display_state
+  p row1.display_values()
+
+end
+
+main()
