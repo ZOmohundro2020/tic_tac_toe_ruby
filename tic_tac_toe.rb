89fa6b39d9651b46452ab136658e8765b5d9a4c1
ZOmohundro <zaneomohundro@gmail.com>
2021-06-04 20:48:52 -0400

    Continue to map out Board and Row classes and general cleanup

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,53 +1,47 @@
 class Board
-  @@state = 'Player1'
-  def display_state
-    puts
-    puts "1 | 2 | 3 "
+  attr_accessor :row
+
+  def initialize(row)
+    @row = row
+  end
+
+  def print_board
+    row
   end
 end
 
 class Row
   def initialize(values_array)
-    #@slot1 = values_array[0]
-    #@slot2 = values_array[1]
-    #@slot3 = values_array[2]
-    p "@slot1 is #{@slot1}"
-    
-    
-  end
-  def display_values
-    # p @slot1
-    #values_array
-    puts "test"
+    (@slot1, @slot2, @slot3) = values_array
   end
 
+  def display_values
+    [@slot1, @slot2, @slot3]
+  end
 end
 
 class Player
   attr_reader :name, :piece
-  def initialize(name,piece)
+
+  def initialize(name, piece)
     @name = name
     @piece = piece
   end
 end
 
+def main
+  top_row = Row.new([7, 8, 9])
+  board = Board.new(top_row)
 
-
-
-def main()
-  board = Board.new()
-  row1 = [7,8,9]
-  
-  player1 = Player.new("Player1","X")
-  player2 = Player.new("Player2","O")
-
+  player1 = Player.new('Player1', 'X')
+  player2 = Player.new('Player2', 'O')
 
   puts "\nWelcome to tic-tac-toe"
   puts "#{player1.name} will start and their piece is '#{player1.piece}'"
 
-  board.display_state
-  p row1.display_values()
-
+  p board.row
+  p top_row.display_values
+  p board.print_board
 end
 
-main()
+main
