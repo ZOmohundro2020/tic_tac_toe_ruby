f46caa790003d34cc05f7f5ee6d1819415b9644a
ZOmohundro <zaneomohundro@gmail.com>
2021-06-05 20:12:11 -0400

    Set up basic output

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,16 +1,25 @@
 class Board
-  attr_accessor :row
 
-  def initialize(row)
-    @row = row
+  def initialize(row1,row2,row3)
+    @row1 = row1
+    @row2 = row2
+    @row3 = row3
   end
 
   def print_board
-    row
+    puts "#{@row1.slot1} | #{@row1.slot2} | #{@row1.slot3}"
+    puts "--+---+--"
+    puts "#{@row2.slot1} | #{@row2.slot2} | #{@row2.slot3}"
+    puts "--+---+--"
+    puts "#{@row3.slot1} | #{@row3.slot2} | #{@row3.slot3}"
+    puts
   end
+
 end
 
 class Row
+  attr_accessor :slot1, :slot2, :slot3
+  
   def initialize(values_array)
     (@slot1, @slot2, @slot3) = values_array
   end
@@ -31,17 +40,19 @@ end
 
 def main
   top_row = Row.new([7, 8, 9])
-  board = Board.new(top_row)
+  mid_row = Row.new([4, 5, 6])
+  bot_row = Row.new([1, 2, 3])
+  board = Board.new(top_row,mid_row,bot_row)
 
   player1 = Player.new('Player1', 'X')
   player2 = Player.new('Player2', 'O')
 
   puts "\nWelcome to tic-tac-toe"
   puts "#{player1.name} will start and their piece is '#{player1.piece}'"
+  puts
 
-  p board.row
-  p top_row.display_values
-  p board.print_board
+  board.print_board
+
 end
 
 main
